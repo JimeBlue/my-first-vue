43f86cce134d3810f636415ae13386e2fe09d41d
Jimena <jimeblue69@gmail.com>
2022-08-12 16:34:36 +0200

    Finish with mouse events

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-//  EXPLANATION: create a vue app
 const app = Vue.createApp({
-  // EXPLANATION: This is the vue object in which we can set up any data or functions to react to events that we want. Below I call the data function and inside of it we return an object. The properties inside this object are accesible throughout our template as variables
   data() {
     return {
-      title: 'the final empire',
-      author: 'Brandon',
+      showBooks: true,
+      title: 'The Way of Kings',
+      author: 'Brandon Sanderson',
       age: 45,
+      //note:   properties for the positon of mouse over my div
+      x: 0,
+      y: 0,
     };
   },
-  // EXPLANATION: Another thing we can specify inside the object is the component template, i.e the tamplate that will be renderes inside #app. To do that, I create a template property, which is a string that contains html. But when the template is to big it is not appropriate to write it here, instead we write inside #app in the html file
-  //   template: '<h2> I am the template</h2>',
-  //
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    // EXPLANATION: when we have an event we automatically get access to an optional event object, which is taken as the first parameter in our function (e).
+    handleEvent(e, data) {
+      console.log('event');
+      console.log(e.type, e);
+      if (data) {
+        console.log(data);
+      }
+    },
+    //note:   with this function I update the position of the mouse over the corresponding div. the offsetX and offsetY are part of the event object.
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
 });
 
-// indicate where the app shoulg be mounted, in this case in the div with the id app
 app.mount('#app');
