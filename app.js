d8fa01fdd73207ab7ddf83c76a60dc7ccef1c3d6
Jimena <jimeblue69@gmail.com>
2022-08-15 15:18:37 +0200

    Finish dynamic classes

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-//  EXPLANATION: create a vue app
 const app = Vue.createApp({
-  // EXPLANATION: This is the vue object in which we can set up any data or functions to react to events that we want. Below I call the data function and inside of it we return an object. The properties inside this object are accesible throughout our template as variables
   data() {
     return {
-      title: 'the final empire',
-      author: 'Brandon',
-      age: 45,
+      showBooks: true,
+      books: [
+        {
+          title: 'name of the wind',
+          author: 'patrick rothfuss',
+          img: 'assets/1.jpg',
+          // EXPLANATION: add attribute to signal whether the book is a favourite or not
+          isFav: true,
+        },
+        {
+          title: 'the way of kings',
+          author: 'brandon sanderson',
+          img: 'assets/2.jpg',
+          isFav: false,
+        },
+        {
+          title: 'the final empire',
+          author: 'brandon sanderson',
+          img: 'assets/3.jpg',
+          isFav: true,
+        },
+      ],
     };
   },
-  // EXPLANATION: Another thing we can specify inside the object is the component template, i.e the tamplate that will be renderes inside #app. To do that, I create a template property, which is a string that contains html. But when the template is to big it is not appropriate to write it here, instead we write inside #app in the html file
-  //   template: '<h2> I am the template</h2>',
-  //
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
 });
 
-// indicate where the app shoulg be mounted, in this case in the div with the id app
 app.mount('#app');
